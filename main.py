da19d3c193f7a75b4d8b46e3a4ffb2d47d966bf5
youngmin yoo <yym3055@gmail.com>
2023-11-19 22:54:06 +0900

    Refactor preprocessing matrix calculation

--- main.py
+++ main.py
@@ -6,6 +6,28 @@ from src.preprocessing.preprocessing import *
 import os
 from src.shape.shape import shape
 
+from concurrent.futures import ProcessPoolExecutor
+
+
+def preprocess_matrix(network, mat_path, preprocessing_export_path):
+    network
+    fps = farthest_first_sampling(network, k=10, exact=False)
+
+    distance_matrix = np.zeros((len(fps), len(fps)))
+    for j in range(len(fps)):
+        for k in range(len(fps)):
+            distance_matrix[j][k] = nx.shortest_path_length(
+                network, fps[j], fps[k], weight="weight"
+            )
+    shape_name = mat_path.split("/")[-1].split(".")[0]
+    shape_export_path = os.path.join(
+        preprocessing_export_path, "pre_geodesic_" + shape_name + ".mat"
+    )
+    shape_obj = shape(shape_name, distance_matrix)
+    shape_obj.save_to_mat(shape_export_path)
+
+    return fps, distance_matrix
+
 
 def main():
     data_path = "data/nonrigid3d/"
@@ -22,22 +44,19 @@ def main():
     fps_samples = []
     if not os.path.exists(preprocessing_export_path):
         os.makedirs(preprocessing_export_path)
-    for i in tqdm(range(len(networks))):
-        network = networks[i]
-        fps = farthest_first_sampling(network, k=10, exact=False)
-        fps_samples.append(fps)
-        distance_matrix = np.zeros((len(fps), len(fps)))
-        for j in range(len(fps)):
-            for k in range(len(fps)):
-                distance_matrix[j][k] = nx.shortest_path_length(
-                    network, fps[j], fps[k], weight="weight"
-                )
-        shape_name = mat_paths[i].split("/")[-1].split(".")[0]
-        shape_export_path = os.path.join(
-            preprocessing_export_path, "pre_geodesic_" + shape_name + ".mat"
-        )
-        shape_obj = shape(shape_name, distance_matrix)
-        shape_obj.save_to_mat(shape_export_path)
+    # for i in tqdm(range(len(networks))):
+    #     network = networks[i]
+    #     mat_path = mat_paths[i]
+    #     fps, distance_matrix = preprocess_matrix(
+    #         network, mat_path, preprocessing_export_path
+    #     )
+    #     fps_samples.append(fps)
+
+    with ProcessPoolExecutor() as executor:
+        for network, mat_path in zip(networks, mat_paths):
+            executor.submit(
+                preprocess_matrix, network, mat_path, preprocessing_export_path
+            )
 
 
 if __name__ == "__main__":
