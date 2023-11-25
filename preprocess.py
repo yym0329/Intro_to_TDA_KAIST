1c02f4ea3ed8a83e68f25e453d88020113fa36f3
youngmin yoo <yym3055@gmail.com>
2023-11-25 19:51:45 +0900

    Refactor preprocessing script to improve performance

--- preprocess.py
+++ preprocess.py
@@ -21,59 +21,13 @@ def main():
             G = load_and_construct_matrix(mat_path)
             networks.append(G)
             mat_paths.append(mat_path)
-
-    fps_samples = []
+    print("Shapes loaded.")
     if not os.path.exists(preprocessing_export_path):
         os.makedirs(preprocessing_export_path)
 
-    # Sequential
-    # for i in tqdm(range(len(networks))):
-    #     network = networks[i]
-    #     mat_path = mat_paths[i]
-    #     fps, distance_matrix = preprocess_matrix(
-    #         network, mat_path, preprocessing_export_path
-    #     )
-    #     fps_samples.append(fps)
-
     batch_preprocess_matrix(
-        networks, mat_paths, preprocessing_export_path, num_workers=10
+        networks, mat_paths, preprocessing_export_path, num_workers=8
     )
-
-    # batch_size = 8
-    # batch = []
-    # num_batches = len(networks) // batch_size + 1
-    # curr_batch = 1
-    # for i in range(len(networks)):
-    #     network = networks[i]
-    #     mat_path = mat_paths[i]
-    #     batch.append((network, mat_path))
-
-    #     if len(batch) == batch_size:
-    #         print(f"Processing batch {curr_batch}/{num_batches}")
-    #         futures = []
-    #         with ProcessPoolExecutor() as executor:
-    #             for network, mat_path in batch:
-    #                 future = executor.submit(
-    #                     preprocess_matrix, network, mat_path, preprocessing_export_path
-    #                 )
-    #                 futures.append(future)
-    #             for future in tqdm(as_completed(futures), total=len(futures)):
-    #                 fps, _ = future.result()
-    #                 fps_samples.append(fps)
-
-    #         batch = []
-    # if len(batch) > 0:
-    #     print(f"Processing batch {curr_batch}/{num_batches}")
-    #     futures = []
-    #     with ProcessPoolExecutor() as executor:
-    #         for network, mat_path in batch:
-    #             future = executor.submit(
-    #                 preprocess_matrix, network, mat_path, preprocessing_export_path
-    #             )
-    #             futures.append(future)
-    #         for future in tqdm(as_completed(futures), total=len(futures)):
-    #             fps, _ = future.result()
-    #             fps_samples.append(fps)
 
 
 if __name__ == "__main__":
