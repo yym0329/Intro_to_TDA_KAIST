7913e1242545728a91a40094c162fe993d1477d3
youngmin yoo <yym3055@gmail.com>
2023-11-25 19:31:16 +0900

    improve preprocess_matrix,  add better progress printing for parallel processing.

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ from concurrent.futures import ProcessPoolExecutor
 
 def preprocess_matrix(network, mat_path, preprocessing_export_path):
     network
-    fps = farthest_first_sampling(network, k=200, exact=False)
+    fps, dG = farthest_first_sampling(network, k=200, exact=False)
 
     distance_matrix = np.zeros((len(fps), len(fps)))
+
     for j in range(len(fps)):
         for k in range(len(fps)):
-            distance_matrix[j][k] = nx.shortest_path_length(
-                network, fps[j], fps[k], weight="weight"
-            )
+            distance_matrix[j][k] = dG[fps[j]][fps[k]]
+
     shape_name = mat_path.split("/")[-1].split(".")[0]
     shape_export_path = os.path.join(
         preprocessing_export_path, "pre_geodesic_" + shape_name + ".mat"
@@ -35,6 +35,7 @@ def main():
     preprocessing_export_path = "data/preprocessing/"
     networks = []
     mat_paths = []
+    print("Loading Shapes...")
     for file in tqdm(os.listdir(data_path)):
         if file.endswith(".mat"):
             mat_path = os.path.join(data_path, file)
@@ -45,6 +46,8 @@ def main():
     fps_samples = []
     if not os.path.exists(preprocessing_export_path):
         os.makedirs(preprocessing_export_path)
+
+    # Sequential
     # for i in tqdm(range(len(networks))):
     #     network = networks[i]
     #     mat_path = mat_paths[i]
@@ -52,12 +55,41 @@ def main():
     #         network, mat_path, preprocessing_export_path
     #     )
     #     fps_samples.append(fps)
+    batch_size = 8
+    batch = []
+    num_batches = len(networks) // batch_size + 1
+    curr_batch = 1
+    for i in range(len(networks)):
+        network = networks[i]
+        mat_path = mat_paths[i]
+        batch.append((network, mat_path))
 
-    with ProcessPoolExecutor() as executor:
-        for network, mat_path in zip(networks, mat_paths):
-            executor.submit(
-                preprocess_matrix, network, mat_path, preprocessing_export_path
-            )
+        if len(batch) == batch_size:
+            print(f"Processing batch {curr_batch}/{num_batches}")
+            futures = []
+            with ProcessPoolExecutor() as executor:
+                for network, mat_path in batch:
+                    future = executor.submit(
+                        preprocess_matrix, network, mat_path, preprocessing_export_path
+                    )
+                    futures.append(future)
+                for future in tqdm(as_completed(futures), total=len(futures)):
+                    fps, _ = future.result()
+                    fps_samples.append(fps)
+
+            batch = []
+    if len(batch) > 0:
+        print(f"Processing batch {curr_batch}/{num_batches}")
+        futures = []
+        with ProcessPoolExecutor() as executor:
+            for network, mat_path in batch:
+                future = executor.submit(
+                    preprocess_matrix, network, mat_path, preprocessing_export_path
+                )
+                futures.append(future)
+            for future in tqdm(as_completed(futures), total=len(futures)):
+                fps, _ = future.result()
+                fps_samples.append(fps)
 
 
 if __name__ == "__main__":
