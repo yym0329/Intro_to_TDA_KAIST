e98043dc884b230e09f10deb921345370279b5f0
youngmin yoo <yym3055@gmail.com>
2023-11-25 20:21:49 +0900

    Refactor preprocessing and shape modules

--- src/shape/shape.py
+++ src/shape/shape.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.io import savemat, loadmat
+import ripser
+import persim
 
 
 class Shape:
@@ -16,6 +18,7 @@ class Shape:
         if type(dm) != np.ndarray:
             raise TypeError("dm must be a numpy array")
         self.dm = dm
+        self.dgms = None
 
     def save_to_mat(self, path):
         savemat(path, {"name": self.name, "dm": self.dm})
@@ -24,3 +27,19 @@ class Shape:
         mat_data = loadmat(path)
         self.name = mat_data["name"][0]
         self.dm = mat_data["dm"]
+
+    def get_VR_diagram(self, max_dim=2):
+        self.dgms = ripser.ripser(self.dm, maxdim=max_dim, distance_matrix=True)["dgms"]
+        return self.dgms
+
+    def plot_diagram(self):
+        if self.dgms is None:
+            self.get_VR_diagram()
+        persim.plot_diagrams(self.dgms, show=True)
+
+
+def load_from_mat(path):
+    mat_data = loadmat(path)
+    name = mat_data["name"][0]
+    dm = mat_data["dm"]
+    return Shape(name, dm)
